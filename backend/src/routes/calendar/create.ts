447bb77a905ae437c24c245cc04e6a4504e3f594
Tomáš Kalina <info@tomaskalina.cz>
2020-01-13 13:12:05 +0100

    Příklad pro vytvoření kalendáře

--- backend/src/routes/calendar/create.ts
+++ backend/src/routes/calendar/create.ts
@@ -1,5 +1,23 @@
 import {Request, Response} from "express";
+const MongoClient = require('mongodb').MongoClient;
 
 export function createCalendar(req: Request, res: Response): any {
-    return res.json({status: "success", message: "create"});
+
+    //https://www.w3schools.com/nodejs/nodejs_mongodb_createcollection.asp
+
+    let url = "mongodb://weny.cz:27017/";
+    MongoClient.connect(url, (err, db)  => {
+        if (err) throw err;
+        let dbo = db.db("vimkde");
+        let myobj = { name: "Company Inc", address: "Highway 37" };
+        dbo.collection("calendars").insertOne(myobj, function(err, response) {
+            if (err){
+                return res.json({status: "error", message: err});
+            }
+            console.log("1 document inserted");
+            db.close();
+            return res.json({status: "success", message: response});
+        });
+    });
+
 }
