5716e6e930cc4396ecdde384402fc724ab334c07
Beran Vitezslav :-) <BeranVi>
2020-01-13 15:58:08 +0100

    o nas

---
+++ frontend/src/Onas.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Avatar from '@material-ui/core/Avatar';
+
+export default function Onas(){
+  return(
+    <div className="Onas" >
+      <Avatar>H</Avatar>
+    </div>
+
+  );
+} 
